aae6013f3a002ef9fa06e2b85c96a3affea5dd43
ghost <gh0stmusc@gmail.com>
2017-10-14 18:45:59 -0400

    Bug Fix: Missing File Error

--- app/src/main/java/tk/samgrogan/pulp/UI/ShortBoxFragment.java
+++ app/src/main/java/tk/samgrogan/pulp/UI/ShortBoxFragment.java
@@ -109,17 +109,22 @@ public class ShortBoxFragment extends Fragment {
             for (int i = 0; i < firePaths.size(); i++){
                 //
                 String file = firePaths.get(i);
-
-                if (file.endsWith(".cbr")){
-                    cbr.read(file);
-                    cbr.getCbr();
-                    File cache = cbr.getBitmapFile(getContext(),1);
-                    comics.setBitmaps(cbr.getBitmap(cache));
+                File check = new File(file);
+                if (!check.exists()){
+                    firePaths.remove(i);
                 }else {
-                    cbz.read(file);
-                    cbz.getCbz();
-                    cbz.CbzComic();
-                    comics.setBitmaps(cbz.getPage(1));
+
+                    if (file.endsWith(".cbr")) {
+                        cbr.read(file);
+                        cbr.getCbr();
+                        File cache = cbr.getBitmapFile(getContext(), 1);
+                        comics.setBitmaps(cbr.getBitmap(cache));
+                    } else {
+                        cbz.read(file);
+                        cbz.getCbz();
+                        cbz.CbzComic();
+                        comics.setBitmaps(cbz.getPage(1));
+                    }
                 }
 
                 cbr.close();
